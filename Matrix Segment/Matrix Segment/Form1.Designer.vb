5259c9065d3938d0e5fd730f01c128637a1702fd
claudfelix <60541334+claudfelix@users.noreply.github.com>
2020-03-14 20:14:16 +0800

    Add files via upload

---
+++ Matrix Segment/Matrix Segment/Form1.Designer.vb
@@ -0,0 +1,357 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.Label16 = New System.Windows.Forms.Label()
+        Me.Label17 = New System.Windows.Forms.Label()
+        Me.Label18 = New System.Windows.Forms.Label()
+        Me.Label19 = New System.Windows.Forms.Label()
+        Me.Label20 = New System.Windows.Forms.Label()
+        Me.Label21 = New System.Windows.Forms.Label()
+        Me.Label22 = New System.Windows.Forms.Label()
+        Me.Label23 = New System.Windows.Forms.Label()
+        Me.Label24 = New System.Windows.Forms.Label()
+        Me.Label25 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(50, 50)
+        Me.Label1.TabIndex = 0
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(68, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(50, 50)
+        Me.Label2.TabIndex = 1
+        '
+        'Label3
+        '
+        Me.Label3.BackColor = System.Drawing.Color.White
+        Me.Label3.Location = New System.Drawing.Point(124, 9)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(50, 50)
+        Me.Label3.TabIndex = 2
+        '
+        'Label4
+        '
+        Me.Label4.BackColor = System.Drawing.Color.White
+        Me.Label4.Location = New System.Drawing.Point(180, 9)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(50, 50)
+        Me.Label4.TabIndex = 3
+        '
+        'Label5
+        '
+        Me.Label5.BackColor = System.Drawing.Color.White
+        Me.Label5.Location = New System.Drawing.Point(236, 9)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(50, 50)
+        Me.Label5.TabIndex = 4
+        '
+        'Label6
+        '
+        Me.Label6.BackColor = System.Drawing.Color.White
+        Me.Label6.Location = New System.Drawing.Point(236, 72)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(50, 50)
+        Me.Label6.TabIndex = 9
+        '
+        'Label7
+        '
+        Me.Label7.BackColor = System.Drawing.Color.White
+        Me.Label7.Location = New System.Drawing.Point(180, 72)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(50, 50)
+        Me.Label7.TabIndex = 8
+        '
+        'Label8
+        '
+        Me.Label8.BackColor = System.Drawing.Color.White
+        Me.Label8.Location = New System.Drawing.Point(124, 72)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(50, 50)
+        Me.Label8.TabIndex = 7
+        '
+        'Label9
+        '
+        Me.Label9.BackColor = System.Drawing.Color.White
+        Me.Label9.Location = New System.Drawing.Point(68, 72)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(50, 50)
+        Me.Label9.TabIndex = 6
+        '
+        'Label10
+        '
+        Me.Label10.BackColor = System.Drawing.Color.White
+        Me.Label10.Location = New System.Drawing.Point(12, 72)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(50, 50)
+        Me.Label10.TabIndex = 5
+        '
+        'Label11
+        '
+        Me.Label11.BackColor = System.Drawing.Color.White
+        Me.Label11.Location = New System.Drawing.Point(236, 133)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(50, 50)
+        Me.Label11.TabIndex = 14
+        '
+        'Label12
+        '
+        Me.Label12.BackColor = System.Drawing.Color.White
+        Me.Label12.Location = New System.Drawing.Point(180, 133)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(50, 50)
+        Me.Label12.TabIndex = 13
+        '
+        'Label13
+        '
+        Me.Label13.BackColor = System.Drawing.Color.White
+        Me.Label13.Location = New System.Drawing.Point(124, 133)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(50, 50)
+        Me.Label13.TabIndex = 12
+        '
+        'Label14
+        '
+        Me.Label14.BackColor = System.Drawing.Color.White
+        Me.Label14.Location = New System.Drawing.Point(68, 133)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(50, 50)
+        Me.Label14.TabIndex = 11
+        '
+        'Label15
+        '
+        Me.Label15.BackColor = System.Drawing.Color.White
+        Me.Label15.Location = New System.Drawing.Point(12, 133)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(50, 50)
+        Me.Label15.TabIndex = 10
+        '
+        'Label16
+        '
+        Me.Label16.BackColor = System.Drawing.Color.White
+        Me.Label16.Location = New System.Drawing.Point(236, 197)
+        Me.Label16.Name = "Label16"
+        Me.Label16.Size = New System.Drawing.Size(50, 50)
+        Me.Label16.TabIndex = 19
+        '
+        'Label17
+        '
+        Me.Label17.BackColor = System.Drawing.Color.White
+        Me.Label17.Location = New System.Drawing.Point(180, 197)
+        Me.Label17.Name = "Label17"
+        Me.Label17.Size = New System.Drawing.Size(50, 50)
+        Me.Label17.TabIndex = 18
+        '
+        'Label18
+        '
+        Me.Label18.BackColor = System.Drawing.Color.White
+        Me.Label18.Location = New System.Drawing.Point(124, 197)
+        Me.Label18.Name = "Label18"
+        Me.Label18.Size = New System.Drawing.Size(50, 50)
+        Me.Label18.TabIndex = 17
+        '
+        'Label19
+        '
+        Me.Label19.BackColor = System.Drawing.Color.White
+        Me.Label19.Location = New System.Drawing.Point(68, 197)
+        Me.Label19.Name = "Label19"
+        Me.Label19.Size = New System.Drawing.Size(50, 50)
+        Me.Label19.TabIndex = 16
+        '
+        'Label20
+        '
+        Me.Label20.BackColor = System.Drawing.Color.White
+        Me.Label20.Location = New System.Drawing.Point(12, 197)
+        Me.Label20.Name = "Label20"
+        Me.Label20.Size = New System.Drawing.Size(50, 50)
+        Me.Label20.TabIndex = 15
+        '
+        'Label21
+        '
+        Me.Label21.BackColor = System.Drawing.Color.White
+        Me.Label21.Location = New System.Drawing.Point(236, 259)
+        Me.Label21.Name = "Label21"
+        Me.Label21.Size = New System.Drawing.Size(50, 50)
+        Me.Label21.TabIndex = 24
+        '
+        'Label22
+        '
+        Me.Label22.BackColor = System.Drawing.Color.White
+        Me.Label22.Location = New System.Drawing.Point(180, 259)
+        Me.Label22.Name = "Label22"
+        Me.Label22.Size = New System.Drawing.Size(50, 50)
+        Me.Label22.TabIndex = 23
+        '
+        'Label23
+        '
+        Me.Label23.BackColor = System.Drawing.Color.White
+        Me.Label23.Location = New System.Drawing.Point(124, 259)
+        Me.Label23.Name = "Label23"
+        Me.Label23.Size = New System.Drawing.Size(50, 50)
+        Me.Label23.TabIndex = 22
+        '
+        'Label24
+        '
+        Me.Label24.BackColor = System.Drawing.Color.White
+        Me.Label24.Location = New System.Drawing.Point(68, 259)
+        Me.Label24.Name = "Label24"
+        Me.Label24.Size = New System.Drawing.Size(50, 50)
+        Me.Label24.TabIndex = 21
+        '
+        'Label25
+        '
+        Me.Label25.BackColor = System.Drawing.Color.White
+        Me.Label25.Location = New System.Drawing.Point(12, 259)
+        Me.Label25.Name = "Label25"
+        Me.Label25.Size = New System.Drawing.Size(50, 50)
+        Me.Label25.TabIndex = 20
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 322)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(274, 44)
+        Me.Button1.TabIndex = 25
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(12, 372)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(274, 44)
+        Me.Button2.TabIndex = 26
+        Me.Button2.Text = "Stop"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(99, 147)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 26)
+        Me.TextBox1.TabIndex = 27
+        Me.TextBox1.Text = "0"
+        Me.TextBox1.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.ClientSize = New System.Drawing.Size(296, 424)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label21)
+        Me.Controls.Add(Me.Label22)
+        Me.Controls.Add(Me.Label23)
+        Me.Controls.Add(Me.Label24)
+        Me.Controls.Add(Me.Label25)
+        Me.Controls.Add(Me.Label16)
+        Me.Controls.Add(Me.Label17)
+        Me.Controls.Add(Me.Label18)
+        Me.Controls.Add(Me.Label19)
+        Me.Controls.Add(Me.Label20)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.Label15)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label14 As Label
+    Friend WithEvents Label15 As Label
+    Friend WithEvents Label16 As Label
+    Friend WithEvents Label17 As Label
+    Friend WithEvents Label18 As Label
+    Friend WithEvents Label19 As Label
+    Friend WithEvents Label20 As Label
+    Friend WithEvents Label21 As Label
+    Friend WithEvents Label22 As Label
+    Friend WithEvents Label23 As Label
+    Friend WithEvents Label24 As Label
+    Friend WithEvents Label25 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ Matrix Segment/Matrix Segment/Form1.vb
@@ -0,0 +1,293 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Timer1.Start()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Timer1.Stop()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        TextBox1.Text += 1
+
+        If TextBox1.Text = 1 Then
+            Label1.BackColor = Color.White
+            Label2.BackColor = Color.Red
+            Label3.BackColor = Color.Red
+            Label4.BackColor = Color.White
+            Label5.BackColor = Color.White
+            Label6.BackColor = Color.White
+            Label7.BackColor = Color.White
+            Label8.BackColor = Color.Red
+            Label9.BackColor = Color.White
+            Label10.BackColor = Color.White
+            Label11.BackColor = Color.White
+            Label12.BackColor = Color.White
+            Label13.BackColor = Color.Red
+            Label14.BackColor = Color.White
+            Label15.BackColor = Color.White
+            Label16.BackColor = Color.White
+            Label17.BackColor = Color.White
+            Label18.BackColor = Color.Red
+            Label19.BackColor = Color.White
+            Label20.BackColor = Color.White
+            Label21.BackColor = Color.Red
+            Label22.BackColor = Color.Red
+            Label23.BackColor = Color.Red
+            Label24.BackColor = Color.Red
+            Label25.BackColor = Color.Red
+        End If
+
+        If TextBox1.Text = 2 Then
+            Label1.BackColor = Color.White
+            Label2.BackColor = Color.Red
+            Label3.BackColor = Color.Red
+            Label4.BackColor = Color.Red
+            Label5.BackColor = Color.Red
+            Label6.BackColor = Color.White
+            Label7.BackColor = Color.Red
+            Label8.BackColor = Color.White
+            Label9.BackColor = Color.White
+            Label10.BackColor = Color.Red
+            Label11.BackColor = Color.White
+            Label12.BackColor = Color.White
+            Label13.BackColor = Color.Red
+            Label14.BackColor = Color.White
+            Label15.BackColor = Color.White
+            Label16.BackColor = Color.White
+            Label17.BackColor = Color.White
+            Label18.BackColor = Color.White
+            Label19.BackColor = Color.Red
+            Label20.BackColor = Color.White
+            Label21.BackColor = Color.Red
+            Label22.BackColor = Color.Red
+            Label23.BackColor = Color.Red
+            Label24.BackColor = Color.Red
+            Label25.BackColor = Color.Red
+        End If
+
+        If TextBox1.Text = 3 Then
+            Label1.BackColor = Color.White
+            Label2.BackColor = Color.Red
+            Label3.BackColor = Color.Red
+            Label4.BackColor = Color.Red
+            Label5.BackColor = Color.White
+            Label6.BackColor = Color.Red
+            Label7.BackColor = Color.White
+            Label8.BackColor = Color.White
+            Label9.BackColor = Color.White
+            Label10.BackColor = Color.Red
+            Label11.BackColor = Color.Red
+            Label12.BackColor = Color.Red
+            Label13.BackColor = Color.Red
+            Label14.BackColor = Color.White
+            Label15.BackColor = Color.White
+            Label16.BackColor = Color.Red
+            Label17.BackColor = Color.White
+            Label18.BackColor = Color.White
+            Label19.BackColor = Color.White
+            Label20.BackColor = Color.Red
+            Label21.BackColor = Color.White
+            Label22.BackColor = Color.Red
+            Label23.BackColor = Color.Red
+            Label24.BackColor = Color.Red
+            Label25.BackColor = Color.White
+        End If
+
+        If TextBox1.Text = 4 Then
+            Label1.BackColor = Color.White
+            Label2.BackColor = Color.White
+            Label3.BackColor = Color.Red
+            Label4.BackColor = Color.White
+            Label5.BackColor = Color.Red
+
+            Label6.BackColor = Color.Red
+            Label7.BackColor = Color.White
+            Label8.BackColor = Color.White
+            Label9.BackColor = Color.Red
+            Label10.BackColor = Color.White
+
+            Label11.BackColor = Color.Red
+            Label12.BackColor = Color.Red
+            Label13.BackColor = Color.Red
+            Label14.BackColor = Color.Red
+            Label15.BackColor = Color.Red
+
+            Label16.BackColor = Color.Red
+            Label17.BackColor = Color.White
+            Label18.BackColor = Color.White
+            Label19.BackColor = Color.White
+            Label20.BackColor = Color.White
+
+            Label21.BackColor = Color.Red
+            Label22.BackColor = Color.White
+            Label23.BackColor = Color.White
+            Label24.BackColor = Color.White
+            Label25.BackColor = Color.White
+        End If
+
+        If TextBox1.Text = 5 Then
+            Label1.BackColor = Color.Red
+            Label2.BackColor = Color.Red
+            Label3.BackColor = Color.Red
+            Label4.BackColor = Color.Red
+            Label5.BackColor = Color.Red
+
+            Label6.BackColor = Color.White
+            Label7.BackColor = Color.White
+            Label8.BackColor = Color.White
+            Label9.BackColor = Color.White
+            Label10.BackColor = Color.Red
+
+            Label11.BackColor = Color.White
+            Label12.BackColor = Color.Red
+            Label13.BackColor = Color.Red
+            Label14.BackColor = Color.Red
+            Label15.BackColor = Color.Red
+
+            Label16.BackColor = Color.Red
+            Label17.BackColor = Color.White
+            Label18.BackColor = Color.White
+            Label19.BackColor = Color.White
+            Label20.BackColor = Color.White
+
+            Label21.BackColor = Color.White
+            Label22.BackColor = Color.Red
+            Label23.BackColor = Color.Red
+            Label24.BackColor = Color.Red
+            Label25.BackColor = Color.Red
+        End If
+
+        If TextBox1.Text = 6 Then
+            Label1.BackColor = Color.White
+            Label2.BackColor = Color.Red
+            Label3.BackColor = Color.Red
+            Label4.BackColor = Color.Red
+            Label5.BackColor = Color.Red
+
+            Label6.BackColor = Color.White
+            Label7.BackColor = Color.White
+            Label8.BackColor = Color.White
+            Label9.BackColor = Color.White
+            Label10.BackColor = Color.Red
+
+            Label11.BackColor = Color.White
+            Label12.BackColor = Color.Red
+            Label13.BackColor = Color.Red
+            Label14.BackColor = Color.Red
+            Label15.BackColor = Color.Red
+
+            Label16.BackColor = Color.Red
+            Label17.BackColor = Color.White
+            Label18.BackColor = Color.White
+            Label19.BackColor = Color.White
+            Label20.BackColor = Color.Red
+
+            Label21.BackColor = Color.White
+            Label22.BackColor = Color.Red
+            Label23.BackColor = Color.Red
+            Label24.BackColor = Color.Red
+            Label25.BackColor = Color.White
+        End If
+
+        If TextBox1.Text = 7 Then
+            Label1.BackColor = Color.Red
+            Label2.BackColor = Color.Red
+            Label3.BackColor = Color.Red
+            Label4.BackColor = Color.Red
+            Label5.BackColor = Color.Red
+
+            Label6.BackColor = Color.White
+            Label7.BackColor = Color.Red
+            Label8.BackColor = Color.White
+            Label9.BackColor = Color.White
+            Label10.BackColor = Color.Red
+
+            Label11.BackColor = Color.White
+            Label12.BackColor = Color.White
+            Label13.BackColor = Color.Red
+            Label14.BackColor = Color.White
+            Label15.BackColor = Color.White
+
+            Label16.BackColor = Color.White
+            Label17.BackColor = Color.White
+            Label18.BackColor = Color.White
+            Label19.BackColor = Color.Red
+            Label20.BackColor = Color.White
+
+            Label21.BackColor = Color.White
+            Label22.BackColor = Color.White
+            Label23.BackColor = Color.White
+            Label24.BackColor = Color.White
+            Label25.BackColor = Color.Red
+        End If
+
+        If TextBox1.Text = 8 Then
+            Label1.BackColor = Color.White
+            Label2.BackColor = Color.Red
+            Label3.BackColor = Color.Red
+            Label4.BackColor = Color.Red
+            Label5.BackColor = Color.White
+
+            Label6.BackColor = Color.Red
+            Label7.BackColor = Color.White
+            Label8.BackColor = Color.White
+            Label9.BackColor = Color.White
+            Label10.BackColor = Color.Red
+
+            Label11.BackColor = Color.White
+            Label12.BackColor = Color.Red
+            Label13.BackColor = Color.Red
+            Label14.BackColor = Color.Red
+            Label15.BackColor = Color.White
+
+            Label16.BackColor = Color.Red
+            Label17.BackColor = Color.White
+            Label18.BackColor = Color.White
+            Label19.BackColor = Color.White
+            Label20.BackColor = Color.Red
+
+            Label21.BackColor = Color.White
+            Label22.BackColor = Color.Red
+            Label23.BackColor = Color.Red
+            Label24.BackColor = Color.Red
+            Label25.BackColor = Color.White
+        End If
+
+        If TextBox1.Text = 9 Then
+            Label1.BackColor = Color.White
+            Label2.BackColor = Color.Red
+            Label3.BackColor = Color.Red
+            Label4.BackColor = Color.Red
+            Label5.BackColor = Color.White
+
+            Label6.BackColor = Color.Red
+            Label7.BackColor = Color.White
+            Label8.BackColor = Color.White
+            Label9.BackColor = Color.White
+            Label10.BackColor = Color.Red
+
+            Label11.BackColor = Color.Red
+            Label12.BackColor = Color.Red
+            Label13.BackColor = Color.Red
+            Label14.BackColor = Color.Red
+            Label15.BackColor = Color.White
+
+            Label16.BackColor = Color.Red
+            Label17.BackColor = Color.White
+            Label18.BackColor = Color.White
+            Label19.BackColor = Color.White
+            Label20.BackColor = Color.White
+
+            Label21.BackColor = Color.White
+            Label22.BackColor = Color.Red
+            Label23.BackColor = Color.Red
+            Label24.BackColor = Color.Red
+            Label25.BackColor = Color.Red
+        End If
+
+        If TextBox1.Text > 9 Then
+            TextBox1.Text = 0
+        End If
+    End Sub
+End Class
